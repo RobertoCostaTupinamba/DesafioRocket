47d987bf5066ca8275ed08e65bf5b577bfc29307
Roberto Costa Tupinambá <robertotupinamba@gmail.com>
2019-08-30 17:36:53 -0300

    Update RocketDesafioJS.js

--- DesafioJS/Desafio1/RocketDesafioJS.js
+++ DesafioJS/Desafio1/RocketDesafioJS.js
@@ -18,13 +18,25 @@ var endereco = {
    
    // 2º exercício
    //Crie uma função que dado um intervalo (entre x e y) exiba todos número pares:
-   
+   var vetorPar = Array();
    function pares(x, y) {
+       
        for(var i=x; i<=y;i++){
            if(i%2==0){
-               console.log(i);
+               vetorPar.push(i);
+               
            }
        }
+       console.log(vetorPar);
    }
    pares(32, 321);
-      
+//    3º exercício
+// Escreva uma função que verifique se o vetor de habilidades passado possui a habilidade "Javascript"
+// e retorna um booleano true/false caso exista ou não.
+
+function temHabilidade(skills) {
+    // skills.forEach(element => {
+    //     console.log(element)
+    // });
+var skills = ["Javascript", "ReactJS", "React Native"];
+temHabilidade(skills); // true ou false
