6fdea2edc3835adb277951d4bea8166acabbe338
Roberto Costa Tupinambá <robertotupinamba@gmail.com>
2019-08-31 09:18:35 -0300

    Exercicio 4 '
    Desafio de js exercicio 4 rockectseat

--- DesafioJS/Desafio1/RocketDesafioJS.js
+++ DesafioJS/Desafio1/RocketDesafioJS.js
@@ -30,7 +30,7 @@ var endereco = {
        console.log(vetorPar);
    }
    pares(32, 321);
-   
+
 //    3º exercício
 // Escreva uma função que verifique se o vetor de habilidades passado possui a habilidade "Javascript"
 // e retorna um booleano true/false caso exista ou não.
@@ -46,4 +46,29 @@ function temHabilidade(skills) {
 }
    var skills = ["Javascript", "ReactJS", "React Native"];
    console.log(temHabilidade(skills)); // true ou false
+
+//    4º exercício
+// Escreva uma função que dado um total de anos de estudo retorna o quão experiente o usuário é:
+
+function experiencia(anos) {
+    if(anos>=0 && anos <=1){
+        console.log("Iniciante");
+    }
+    else if(anos>=1 && anos <=3){
+        console.log("Intermediario");
+    }
+    else if(anos>=3 && anos<=6){
+        console.log("Avançado");
+    }
+    else{
+        console.log("Jedi Master");
+    }
+}
+   var anosEstudo =7;
+   experiencia(anosEstudo);
+   // De 0-1 ano: Iniciante
+   // De 1-3 anos: Intermediário
+   // De 3-6 anos: Avançado
+   // De 7 acima: Jedi Master
+   
    
